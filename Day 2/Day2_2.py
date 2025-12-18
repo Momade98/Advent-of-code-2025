cc09880746b46a890f4a449148bac766cfb6a4a4
mberrahou <bemomoh@hotmail.com>
2025-12-18 16:41:43 +0100

    finished Part 2 of Day 2

--- Day 2/Day2_2.py
+++ Day 2/Day2_2.py
@@ -1,4 +1,6 @@
-with open("./testinput.txt", "r") as file:
+import textwrap
+
+with open("./input.txt", "r") as file:
     inputdata = file.read().split(",")
 
 total = 0
@@ -6,15 +8,26 @@ for sequence in inputdata:
     sequence = sequence.split("-")
     sequence = list(map(int, sequence))
 
-    # print(range[1] + 1)
-
+    invalid_id = []
     for id in range(sequence[0], sequence[1] + 1):
-        counter = 0
         strid = str(id)
         if len(strid) > 1:
-            for division in range(2, len(strid)):
+            for division in range(2, len(strid) + 1):
                 if len(strid) % division == 0:
-                    #divide the string in equal parts and compare them (just compare the first part of the string (array) to the rest of the array)
+                    idSeparated = textwrap.wrap(strid, len(strid) // division)
+                    # print(idSeparated[0])
+                    # print(idSeparated)
+                    counter = 0
+                    for parOfId in idSeparated:
+                        if idSeparated[0] != parOfId:
+                            counter += 1
+                    if counter == 0 and id not in invalid_id:
+                        invalid_id.append(id)
+                        # print(id)
+    for id in invalid_id:
+        total += id
+
+print(total)
 
 
 
