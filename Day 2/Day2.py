9422910fa8a494ec22809d6e9cafa051f463b2a4
Momade98 <43603238+Momade98@users.noreply.github.com>
2025-12-18 20:48:28 +0100

    cleaned to uselss code from day 2

--- Day 2/Day2.py
+++ Day 2/Day2.py
@@ -1,24 +1,11 @@
 with open("./input.txt", "r") as file:
     inputdata = file.read().split(",")
-
-# print (inputdata)
-# print(inputdata[0].split("-"))
-
-
-# test = 1188511885
-# test = str(test)
-# if len(test) % 2 == 0:
-#     print(test[:len(test) // 2])
-
 total = 0
 for sequence in inputdata:
     sequence = sequence.split("-")
     sequence = list(map(int, sequence))
 
-    # print(range[1] + 1)
-
     for id in range(sequence[0], sequence[1] + 1):
-        # print(id)
         strid = str(id)
         if len(strid) % 2 == 0:
             if strid[:len(strid) // 2] == strid[len(strid) // 2:]:
--- Day 2/Day2_2.py
+++ Day 2/Day2_2.py
@@ -15,27 +15,13 @@ for sequence in inputdata:
             for division in range(2, len(strid) + 1):
                 if len(strid) % division == 0:
                     idSeparated = textwrap.wrap(strid, len(strid) // division)
-                    # print(idSeparated[0])
-                    # print(idSeparated)
                     counter = 0
                     for parOfId in idSeparated:
                         if idSeparated[0] != parOfId:
                             counter += 1
                     if counter == 0 and id not in invalid_id:
                         invalid_id.append(id)
-                        # print(id)
     for id in invalid_id:
         total += id
 
-print(total)
-
-
-
-##################################################################################
-        #     for division in range(2, len(strid) + 1):
-        #         if len(strid) % division == 0:
-        #             for indexPartOfId in range(len(strid), division):
-        #                 if strid[:len(strid) // division] != strid[len(strid) // indexPartOfId:]:
-        #                     counter += 1
-        # if counter > 0:
-        #     total += id                    
+print(total)         
