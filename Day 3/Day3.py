211a903dabca73247a3d584221c86252326215cd
Momade98 <43603238+Momade98@users.noreply.github.com>
2025-12-19 14:38:13 +0100

    Completed part 1 of Day 3. Beginning of part 2

--- Day 3/Day3.py
+++ Day 3/Day3.py
@@ -1,19 +1,15 @@
-with open("./testinput.txt", "r") as file:
+with open("./input.txt", "r") as file:
     inputdata = file.read().splitlines()
 
 
 total = 0
 for bank in inputdata:
-    str_totalvoltage = ""
-    biggest = [0 , "index"]
-    for i, strbattery in enumerate(bank):
-        battery = int(strbattery)
-        if battery > biggest[0]:
-            biggest[0] = battery
-            biggest[1] = i
-    second_battery = battery.pop(biggest[1])
-    for i, strbattery in enumerate(second_battery):
-        battery = int(strbattery)
-        if battery > biggest[0]:
-            biggest[0] = battery
-            biggest[1] = i
+    biggest = 0
+    for x, battery in enumerate(bank):
+        for y, secondBattery in enumerate(bank):
+            if y > x:
+                if int(bank[x] + bank[y]) > biggest:
+                    biggest = int(bank[x] + bank[y])
+    total += biggest
+
+print(total)
